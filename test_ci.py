e17498d1c079f52b0e225bcae163c45e9d7e9e96
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2022-01-26 18:45:13 -0700

    changed test_ci

--- test_ci.py
+++ test_ci.py
@@ -5,9 +5,10 @@ import json
 
 def test_health_check():
     r = requests.get("http://localhost:8085")
-    assert (r.status_code == 404)
+    assert(r.status_code == 404)
+
 
 def test_root_message():
     r = requests.get("http://localhost:8085")
     res = r.json()
-    assert(res["message"] == "HTTP 404 not found")
+    assert(res["message"] == "HTTP 404 Not Found")
